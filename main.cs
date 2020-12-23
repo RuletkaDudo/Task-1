dd460115ce6a9803e7570584cb5c88cf3633dd03
Ruletka <Ruletka.com@yandex.ru>
2020-12-23 21:00:57 +0300

    sort + gitignore

--- main.cs
+++ main.cs
@@ -4,6 +4,12 @@ public class Program
 {
 	public static void Main()
 	{
-		Console.WriteLine("Hello World");
-	}
+        Console.WriteLine("Hello World");
+        int[] mas = { 1, 5, 2, 4, 9, 0, 3 };
+        Array.Sort(mas);
+        foreach (int i in mas)
+        {
+            Console.Write(i + " ");
+        }
+    }
 }
